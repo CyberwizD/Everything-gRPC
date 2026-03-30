3fe7ab79a959b9fd5ae3286954e855cfb04877a4
WI$DOM <141811303+CyberwizD@users.noreply.github.com>
2026-03-30 09:43:31 +0100

    feat: initialize and register chat service

--- server/server.go
+++ server/server.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net"
 
+	"github.com/CyberwizD/Everything-gRPC/chat"
 	"google.golang.org/grpc"
 )
 
@@ -13,14 +14,18 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to listen, %v", err)
 	}
-	
+
+	// Initialize chat server
+	chatServer := &chat.Server{}
+
 	// Initialize gRPC server
 	grpcServer := grpc.NewServer()
 
+	// Register chat service server
+	chat.RegisterChatServiceServer(grpcServer, chatServer)
+
 	// Serve gRPC server on port 9000
-	if err := grpcServer.Serve(port);
-	err != nil {
+	if err := grpcServer.Serve(port); err != nil {
 		log.Fatalf("Failed to serve gRPC comm, %v", err)
 	}
 }
-
