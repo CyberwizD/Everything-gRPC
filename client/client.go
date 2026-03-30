38166f779f4e2a0b2673e3c57ce1d34910316276
WI$DOM <141811303+CyberwizD@users.noreply.github.com>
2026-03-30 03:32:23 +0100

    feat: implement gRPC client

--- client/client.go
+++ client/client.go
@@ -1,1 +1,38 @@
-package client
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/CyberwizD/Everything-gRPC/chat"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	var connect *grpc.ClientConn
+
+	// Initialize new client with disabled transport security
+	connect, err := grpc.NewClient(":9000", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Failed to connect: %v", err)
+	}
+
+	defer connect.Close()
+
+	// Initialize new chat client
+	chatClient := chat.NewChatServiceClient(connect)
+
+	// Client message
+	message := &chat.Chat {
+		Body: "Message from the client",
+	}
+
+	// Return gRPC client reponse
+	response, err := chatClient.Response(context.Background(), message)
+	if err != nil {
+		log.Fatalf("Failed to send client response, %v", err)
+	}
+
+	log.Printf("Response from server: %s", response.Body)
+}
